4f7d4a9c01843fb06f246f3f0b017d71d79aa49a
Brandsma <13336004+Brandsma@users.noreply.github.com>
2021-02-26 17:58:54 +0100

    Started on hotbar switching

--- MuchBetterHotkeys/MuchBetterHotkeys.cs
+++ MuchBetterHotkeys/MuchBetterHotkeys.cs
@@ -65,12 +65,48 @@
             return ___m_buildPieces;
         }
 
+        private static void SwitchHotbar(Player player) {
+            // TODO: Currently tools don't seem to switch places
+            // TODO: Feathers moved over my tool once and now the tool is gone
+            Debug.Log("Switching hotbars");
+            ref Inventory inv = ref ((Humanoid)player).m_inventory;
+            int width = inv.GetWidth();
+            for (int x = 0; x < width; x++) {
+                ItemDrop.ItemData row1Item = inv.GetItemAt(x, 0);
+                ItemDrop.ItemData row2Item = inv.GetItemAt(x, 1);
+                if (row2Item != null) {
+                    // Row 2 -> Row 1
+                    //inv.RemoveItem(x);
+                    inv.MoveItemToThis(inv, row2Item, row2Item.m_stack, x, 0);
+                }
+                if (row1Item != null) {
+                    // Row 1 -> Row 2
+                    //inv.RemoveItem(width + x);
+                    inv.MoveItemToThis(inv, row1Item, row1Item.m_stack, x, 1);
+                }
+            }
+            return;
+        }
+
         [HarmonyPostfix]
         [HarmonyPatch(typeof(Player), "Update")]
-        private static void Postfix_Update(Player __instance) {
+        private static void Postfix_Update(Player __instance, ref int ___m_placeRotation) {
 
             // We should be the local player and we should be able to take input
             if (Player.m_localPlayer != __instance || !(bool)typeof(Player).GetMethod("TakeInput", BindingFlags.NonPublic | BindingFlags.Instance).Invoke(__instance, new object[0])) {
+                return;
+            }
+
+            if (Input.GetKeyDown(KeyCode.K)) {
+                // TODO: Remove, clears inventory for debugging
+                // Lookout, permanently destroys your inventory
+                ref Inventory inv = ref ((Humanoid)__instance).m_inventory;
+                inv.RemoveAll();
+            }
+
+            //if ((Input.GetKeyDown(KeyCode.LeftShift) || Input.GetKeyDown(KeyCode.RightShift)) && Input.GetKeyDown(KeyCode.Alpha1)) {
+            if (Input.GetKeyDown(KeyCode.Z)) {
+                PlayerHotkeyPatch.SwitchHotbar(__instance);
                 return;
             }
 
@@ -86,24 +122,37 @@
                 if (currentHoverPiece == null) {
                     return;
                 }
+                bool objectFound = false;
                 // Loop over all available pieces until we have the same as what we hover over
-                for (int x = 0; x < availablePieces.Count(); ++x) {
+                for (int x = 0; x < availablePieces.Count() && !objectFound; ++x) {
                     List<Piece> category = availablePieces.ElementAt(x);
-                    for (int y = 0; y < category.Count(); ++y) {
+                    for (int y = 0; y < category.Count() && !objectFound; ++y) {
                         Piece elem = category.ElementAt(y);
                         if (elem.m_name == currentHoverPiece.m_name) {
                             // Set the selected category and selected piece in the piece table of the player
                             current_piece_table.SetCategory((int)elem.m_category);
                             // TODO: Check what the length of the grid row is
-                            current_piece_table.SetSelected(new Vector2Int(y % 10, y / 10));
+                            // current_piece_table.SetSelected(new Vector2Int(y % 10, y / 10));
+                            __instance.SetSelectedPiece(new Vector2Int(y % 10, y / 10));
+
+                            Debug.Log(___m_placeRotation);
+                            Debug.Log(elem.transform.rotation.y);
+                            //___m_placeRotation = (int)(elem.transform.rotation.y / 22.5f);
+
 
                             // Update the placement ghost
                             MethodInfo setupPlacementGhostRef = typeof(Player).GetMethod("SetupPlacementGhost", BindingFlags.NonPublic | BindingFlags.Instance);
                             setupPlacementGhostRef.Invoke(__instance, new object[] { });
+
+                            Debug.Log(elem);
+                            Hud.instance.SetupPieceInfo(elem);
+
+                            objectFound = true;
                         }
                     }
                 }
             }
+            return;
         }
     }
 }
