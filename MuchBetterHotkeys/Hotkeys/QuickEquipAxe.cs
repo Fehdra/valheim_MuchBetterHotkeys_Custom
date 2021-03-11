f50e2761bf5fceb76e04dc85f49e614785504739
Mantsje <gamesedser@hotmail.com>
2021-03-11 22:53:34 +0100

    make quick selecting trigger animations

--- MuchBetterHotkeys/Hotkeys/QuickEquipAxe.cs
+++ MuchBetterHotkeys/Hotkeys/QuickEquipAxe.cs
@@ -1,21 +1,19 @@
-namespace MuchBetterHotkeys
-{
+namespace MuchBetterHotkeys {
 
-    using System;
-    using System.Collections.Generic;
+	using System;
+	using System.Collections.Generic;
 
-    public partial class PlayerHotkeyPatch
-    {
-        private static void QuickEquipAxe(Player player) {
-            if (Hud.IsPieceSelectionVisible()) {
-                return;
-            }
-            Predicate<ItemDrop.ItemData> isTorch = delegate (ItemDrop.ItemData item) { return item.m_shared.m_name == "$item_torch"; };
-            List<ItemDrop.ItemData> torches = player.m_inventory.m_inventory.FindAll(isTorch);
-            torches.Sort(new DurabilityComparer());
-            if (torches.Count > 0) {
-                player.EquipItem(torches[0]);
-            }
-        }
-    }
+	public partial class PlayerHotkeyPatch {
+		private static void QuickEquipAxe(Player player) {
+			if (Hud.IsPieceSelectionVisible()) {
+				return;
+			}
+			Predicate<ItemDrop.ItemData> isTorch = delegate (ItemDrop.ItemData item) { return item.m_shared.m_name == "$item_torch"; };
+			List<ItemDrop.ItemData> torches = player.m_inventory.m_inventory.FindAll(isTorch);
+			torches.Sort(new DurabilityComparer());
+			if (torches.Count > 0) {
+				player.QueueEquipItem(torches[0]);
+			}
+		}
+	}
 }
