11c159a361221ecb0bfa72114f993223a0175724
Mantsje <gamesedser@hotmail.com>
2021-03-12 13:49:01 +0100

    add picking the least, yet still durable toptier axe from inventory (pick)axe. Fix hovering nullpointer exception when non-hoverable object is cast to hoverable

--- MuchBetterHotkeys/Hotkeys/QuickEquipPickaxe.cs
+++ MuchBetterHotkeys/Hotkeys/QuickEquipPickaxe.cs
@@ -1,28 +1,35 @@
-namespace MuchBetterHotkeys
-{
-    using UnityEngine;
-    using System;
-    using System.Collections.Generic;
+namespace MuchBetterHotkeys {
+	using UnityEngine;
+	using System;
+	using System.Linq;
+	using System.Collections.Generic;
 
-    public partial class PlayerHotkeyPatch
-    {
-        public static string[] pickaxeCompareTexts = new string[] { "Rock", "Tin deposit", "Muddy scraps", "Copper deposit", "Obsidian", "Silver vein" };
+	public partial class PlayerHotkeyPatch {
+		public static string[] pickaxeCompareTexts = new string[] { "Rock", "Tin deposit", "Muddy scraps", "Copper deposit", "Obsidian deposit", "Silver vein" };
 
-        private static bool QuickEquipPickaxe(Player player) {
-            GameObject hoverObject = player.GetHoverObject();
-            Hoverable hoverable = (hoverObject ? hoverObject.GetComponentInParent<Hoverable>() : null);
-            string hoverText = hoverable.GetHoverText();
-            if (Array.Exists(pickaxeCompareTexts, element => element == hoverText)) {
-                Predicate<ItemDrop.ItemData> isPickaxe = delegate (ItemDrop.ItemData item) { return item.m_shared.m_name.Contains("$item_pickaxe"); };
-                List<ItemDrop.ItemData> pickaxes = player.m_inventory.m_inventory.FindAll(isPickaxe);
-                // TODO: Check for the best quality pickaxe
-                pickaxes.Sort(new DurabilityComparer());
-                if (pickaxes.Count > 0) {
-                    player.EquipItem(pickaxes[0]);
-                    return false;
-                }
-            }
-            return true;
-        }
-    }
+		private static bool QuickEquipPickaxe(Player player) {
+			GameObject hoverObject = player.GetHoverObject();
+			Hoverable hoverable = ( hoverObject ? hoverObject.GetComponentInParent<Hoverable>() : null );
+			if (hoverable == null) {
+				return true;
+			}
+			string hoverText = hoverable.GetHoverText();
+			if (Array.Exists(pickaxeCompareTexts, element => element == hoverText)) {
+				Predicate<ItemDrop.ItemData> isPickaxe = delegate (ItemDrop.ItemData item) { return item.m_shared.m_name.Contains("$item_pickaxe"); };
+				List<ItemDrop.ItemData> pickaxes = player.m_inventory.m_inventory.FindAll(isPickaxe);
+				List<ItemDrop.ItemData> durablePickaxes = pickaxes.Where(pick => pick.m_durability != 0).ToList();
+				if (durablePickaxes.Count > 0) {
+					int maxTier = durablePickaxes.Max(pick => pick.m_shared.m_toolTier);
+					List<ItemDrop.ItemData> topTierPickaxes = durablePickaxes.Where(pick => pick.m_shared.m_toolTier == maxTier).ToList();
+					topTierPickaxes.Sort(new DurabilityComparer());
+					if (topTierPickaxes.Count > 0) {
+						player.QueueUnequipItem(player.m_rightItem);
+						player.QueueEquipItem(topTierPickaxes[0]);
+						return false;
+					}
+				}
+			}
+			return true;
+		}
+	}
 }
