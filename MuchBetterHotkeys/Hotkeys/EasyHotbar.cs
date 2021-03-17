5efcaa01d7ed691289b5fbb2fcc0260cb7436b1c
Brandsma <13336004+Brandsma@users.noreply.github.com>
2021-03-17 20:26:53 +0100

    Fixed easy hotbar

--- MuchBetterHotkeys/Hotkeys/EasyHotbar.cs
+++ MuchBetterHotkeys/Hotkeys/EasyHotbar.cs
@@ -1,21 +1,23 @@
-﻿namespace MuchBetterHotkeys {
+﻿namespace MuchBetterHotkeys
+{
 
-	using UnityEngine;
+    using UnityEngine;
 
-	public partial class PlayerHotkeyPatch {
-		private static bool EasyHotbar(Player player) {
-			if (!MuchBetterHotkeys.easyHotbarPrefix.Value.IsPressed()) {
-				return true;
-			}
-			KeyCode[] keycodes = { KeyCode.Alpha1, KeyCode.Alpha2, KeyCode.Alpha3, KeyCode.Alpha4 };
-			for (int idx = 0; idx < keycodes.Length; idx++) {
-				KeyCode curKeyCode = keycodes[idx];
-				if (Input.GetKeyDown(curKeyCode)) {
-					player.UseHotbarItem(idx + 5);
-					return false;
-				}
-			}
-			return true;
-		}
-	}
+    public partial class PlayerHotkeyPatch
+    {
+        private static bool EasyHotbar(Player player) {
+            if (!Input.GetKey(MuchBetterHotkeys.easyHotbarPrefix.Value.MainKey)) {
+                return true;
+            }
+            KeyCode[] keycodes = { KeyCode.Alpha1, KeyCode.Alpha2, KeyCode.Alpha3, KeyCode.Alpha4 };
+            for (int idx = 0; idx < keycodes.Length; idx++) {
+                KeyCode curKeyCode = keycodes[idx];
+                if (Input.GetKeyDown(curKeyCode)) {
+                    player.UseHotbarItem(idx + 5);
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
 }
